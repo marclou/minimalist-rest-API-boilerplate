63d6565223cf8c70f1e3e3c89ab2698f12802c94
Marc Lou <marc.louvion@gmail.com>
2022-01-14 22:36:01 +0800

    add gmail transport for emails

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,8 @@ dotenv.config({ path: path.join(__dirname, '../../.env') });
 const envVarsSchema = Joi.object()
   .keys({
     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
-    PORT: Joi.number().default(3000),
+    APP_URL: Joi.string().required().description('URL of the app'),
+    PORT: Joi.number().default(8080),
     MONGODB_URL: Joi.string().required().description('Mongo DB url'),
     JWT_SECRET: Joi.string().required().description('JWT secret key'),
     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
@@ -18,8 +19,6 @@ const envVarsSchema = Joi.object()
     JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: Joi.number()
       .default(10)
       .description('minutes after which verify email token expires'),
-    SMTP_HOST: Joi.string().description('server that will send the emails'),
-    SMTP_PORT: Joi.number().description('port to connect to the email server'),
     SMTP_USERNAME: Joi.string().description('username for email server'),
     SMTP_PASSWORD: Joi.string().description('password for email server'),
     EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
@@ -34,6 +33,7 @@ if (error) {
 
 module.exports = {
   env: envVars.NODE_ENV,
+  appUrl: envVars.APP_URL,
   port: envVars.PORT,
   mongoose: {
     url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
@@ -50,9 +50,8 @@ module.exports = {
     verifyEmailExpirationMinutes: envVars.JWT_VERIFY_EMAIL_EXPIRATION_MINUTES,
   },
   email: {
-    smtp: {
-      host: envVars.SMTP_HOST,
-      port: envVars.SMTP_PORT,
+    nodemailer: {
+      service: 'gmail',
       auth: {
         user: envVars.SMTP_USERNAME,
         pass: envVars.SMTP_PASSWORD,
--- src/services/email.service.js
+++ src/services/email.service.js
@@ -2,13 +2,15 @@ const nodemailer = require('nodemailer');
 const config = require('../config/config');
 const logger = require('../config/logger');
 
-const transport = nodemailer.createTransport(config.email.smtp);
+const transport = nodemailer.createTransport(config.email.nodemailer);
 /* istanbul ignore next */
 if (config.env !== 'test') {
   transport
     .verify()
     .then(() => logger.info('Connected to email server'))
-    .catch(() => logger.warn('Unable to connect to email server. Make sure you have configured the SMTP options in .env'));
+    .catch((e) =>
+      logger.warn(`Unable to connect to email server. Make sure you have configured the SMTP options in .env > ${e}`)
+    );
 }
 
 /**
@@ -30,12 +32,17 @@ const sendEmail = async (to, subject, text) => {
  * @returns {Promise}
  */
 const sendResetPasswordEmail = async (to, token) => {
-  const subject = 'Reset password';
+  const subject = 'Reset your password';
   // replace this url with the link to the reset password page of your front-end app
-  const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
-  const text = `Dear user,
-To reset your password, click on this link: ${resetPasswordUrl}
-If you did not request any password resets, then ignore this email.`;
+  const resetPasswordUrl = `${config.appUrl}reset-password?token=${token}`;
+  const text = `Hey there,
+
+You can reset your password using this link: ${resetPasswordUrl}
+
+If you did not request any password resets, then ignore this email.
+
+Best,
+Marc`;
   await sendEmail(to, subject, text);
 };
 
@@ -46,12 +53,17 @@ If you did not request any password resets, then ignore this email.`;
  * @returns {Promise}
  */
 const sendVerificationEmail = async (to, token) => {
-  const subject = 'Email Verification';
+  const subject = 'Verify your account';
   // replace this url with the link to the email verification page of your front-end app
-  const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
-  const text = `Dear user,
-To verify your email, click on this link: ${verificationEmailUrl}
-If you did not create an account, then ignore this email.`;
+  const verificationEmailUrl = `${config.appUrl}verify-email?token=${token}`;
+  const text = `Hey there,
+
+You can verify your email with this link: ${verificationEmailUrl}
+
+If you did not create an account, then ignore this email.
+
+Best,
+Marc`;
   await sendEmail(to, subject, text);
 };
 
